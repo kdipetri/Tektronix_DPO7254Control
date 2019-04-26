cebfa11373c5f8ed6def8c4c40aef7f02557cfab
Nicola Minafra <nicola.minafra@cern.ch>
2019-04-26 14:13:42 -0500

    Modified ttree structure for test beam May 2019

--- TimingAnalysis/TimingAnalysis/include/pulse.h
+++ TimingAnalysis/TimingAnalysis/include/pulse.h
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////
 // This class has been automatically generated on
-// Fri Nov  9 19:50:29 2018 by ROOT version 6.06/08
-// from TTree pulse/Test beam samples
-// found on file: test_run8.root
+// Fri Apr 26 14:09:46 2019 by ROOT version 6.12/06
+// from TTree pulse/Digitized waveforms
+// found on file: run_scope7729_converted.root
 //////////////////////////////////////////////////////////
 
 #ifndef pulse_h
@@ -23,13 +23,83 @@ public :
 
    // Declaration of leaf types
    UInt_t          i_evt;
-   Float_t         channel[4][1000];
-   Float_t         time[1][1000];
+   Float_t         channel[4][1600];
+   Float_t         time[1][1600];
+   Float_t         baseline[4];
+   Float_t         baseline_RMS[4];
+   Float_t         noise[4];
+   Float_t         amp[4];
+   Float_t         t_peak[4];
+   Float_t         integral[4];
+   Float_t         intfull[4];
+   Float_t         risetime[4];
+   Float_t         decaytime[4];
+   Float_t         LP2_5[4];
+   Float_t         LP2_10[4];
+   Float_t         LP2_15[4];
+   Float_t         LP2_20[4];
+   Float_t         LP2_25[4];
+   Float_t         LP2_30[4];
+   Float_t         LP2_35[4];
+   Float_t         LP2_40[4];
+   Float_t         LP2_45[4];
+   Float_t         LP2_50[4];
+   Float_t         LP2_55[4];
+   Float_t         LP2_60[4];
+   Float_t         LP2_65[4];
+   Float_t         LP2_30mV[4];
+   Float_t         gaus_mean[4];
+   Float_t         gaus_sigma[4];
+   Float_t         gaus_chi2[4];
+   Float_t         xIntercept;
+   Float_t         yIntercept;
+   Float_t         xSlope;
+   Float_t         ySlope;
+   Float_t         x_dut[3];
+   Float_t         y_dut[3];
+   Float_t         chi2;
+   Int_t           ntracks;
+   Int_t           nplanes;
 
    // List of branches
    TBranch        *b_i_evt;   //!
    TBranch        *b_channel;   //!
    TBranch        *b_time;   //!
+   TBranch        *b_baseline;   //!
+   TBranch        *b_baseline_RMS;   //!
+   TBranch        *b_noise;   //!
+   TBranch        *b_amp;   //!
+   TBranch        *b_t_peak;   //!
+   TBranch        *b_integral;   //!
+   TBranch        *b_intfull;   //!
+   TBranch        *b_risetime;   //!
+   TBranch        *b_decaytime;   //!
+   TBranch        *b_LP2_5;   //!
+   TBranch        *b_LP2_10;   //!
+   TBranch        *b_LP2_15;   //!
+   TBranch        *b_LP2_20;   //!
+   TBranch        *b_LP2_25;   //!
+   TBranch        *b_LP2_30;   //!
+   TBranch        *b_LP2_35;   //!
+   TBranch        *b_LP2_40;   //!
+   TBranch        *b_LP2_45;   //!
+   TBranch        *b_LP2_50;   //!
+   TBranch        *b_LP2_55;   //!
+   TBranch        *b_LP2_60;   //!
+   TBranch        *b_LP2_65;   //!
+   TBranch        *b_LP2_30mV;   //!
+   TBranch        *b_gaus_mean;   //!
+   TBranch        *b_gaus_sigma;   //!
+   TBranch        *b_gaus_chi2;   //!
+   TBranch        *b_xIntercept;   //!
+   TBranch        *b_yIntercept;   //!
+   TBranch        *b_xSlope;   //!
+   TBranch        *b_ySlope;   //!
+   TBranch        *b_x_dut;   //!
+   TBranch        *b_y_dut;   //!
+   TBranch        *b_chi2;   //!
+   TBranch        *b_ntracks;   //!
+   TBranch        *b_nplanes;   //!
 
    pulse(TTree *tree=0);
    virtual ~pulse();
@@ -45,14 +115,14 @@ public :
 #endif
 
 #ifdef pulse_cxx
-pulse::pulse(TTree *tree) : fChain(0)
+pulse::pulse(TTree *tree) : fChain(0) 
 {
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("test_run8.root");
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("run_scope7729_converted.root");
       if (!f || !f->IsOpen()) {
-         f = new TFile("test_run8.root");
+         f = new TFile("run_scope7729_converted.root");
       }
       f->GetObject("pulse",tree);
 
@@ -104,6 +174,41 @@ void pulse::Init(TTree *tree)
    fChain->SetBranchAddress("i_evt", &i_evt, &b_i_evt);
    fChain->SetBranchAddress("channel", channel, &b_channel);
    fChain->SetBranchAddress("time", time, &b_time);
+   fChain->SetBranchAddress("baseline", baseline, &b_baseline);
+   fChain->SetBranchAddress("baseline_RMS", baseline_RMS, &b_baseline_RMS);
+   fChain->SetBranchAddress("noise", noise, &b_noise);
+   fChain->SetBranchAddress("amp", amp, &b_amp);
+   fChain->SetBranchAddress("t_peak", t_peak, &b_t_peak);
+   fChain->SetBranchAddress("integral", integral, &b_integral);
+   fChain->SetBranchAddress("intfull", intfull, &b_intfull);
+   fChain->SetBranchAddress("risetime", risetime, &b_risetime);
+   fChain->SetBranchAddress("decaytime", decaytime, &b_decaytime);
+   fChain->SetBranchAddress("LP2_5", LP2_5, &b_LP2_5);
+   fChain->SetBranchAddress("LP2_10", LP2_10, &b_LP2_10);
+   fChain->SetBranchAddress("LP2_15", LP2_15, &b_LP2_15);
+   fChain->SetBranchAddress("LP2_20", LP2_20, &b_LP2_20);
+   fChain->SetBranchAddress("LP2_25", LP2_25, &b_LP2_25);
+   fChain->SetBranchAddress("LP2_30", LP2_30, &b_LP2_30);
+   fChain->SetBranchAddress("LP2_35", LP2_35, &b_LP2_35);
+   fChain->SetBranchAddress("LP2_40", LP2_40, &b_LP2_40);
+   fChain->SetBranchAddress("LP2_45", LP2_45, &b_LP2_45);
+   fChain->SetBranchAddress("LP2_50", LP2_50, &b_LP2_50);
+   fChain->SetBranchAddress("LP2_55", LP2_55, &b_LP2_55);
+   fChain->SetBranchAddress("LP2_60", LP2_60, &b_LP2_60);
+   fChain->SetBranchAddress("LP2_65", LP2_65, &b_LP2_65);
+   fChain->SetBranchAddress("LP2_30mV", LP2_30mV, &b_LP2_30mV);
+   fChain->SetBranchAddress("gaus_mean", gaus_mean, &b_gaus_mean);
+   fChain->SetBranchAddress("gaus_sigma", gaus_sigma, &b_gaus_sigma);
+   fChain->SetBranchAddress("gaus_chi2", gaus_chi2, &b_gaus_chi2);
+   fChain->SetBranchAddress("xIntercept", &xIntercept, &b_xIntercept);
+   fChain->SetBranchAddress("yIntercept", &yIntercept, &b_yIntercept);
+   fChain->SetBranchAddress("xSlope", &xSlope, &b_xSlope);
+   fChain->SetBranchAddress("ySlope", &ySlope, &b_ySlope);
+   fChain->SetBranchAddress("x_dut", x_dut, &b_x_dut);
+   fChain->SetBranchAddress("y_dut", y_dut, &b_y_dut);
+   fChain->SetBranchAddress("chi2", &chi2, &b_chi2);
+   fChain->SetBranchAddress("ntracks", &ntracks, &b_ntracks);
+   fChain->SetBranchAddress("nplanes", &nplanes, &b_nplanes);
    Notify();
 }
 
