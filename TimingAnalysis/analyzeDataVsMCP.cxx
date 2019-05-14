1a16fc3c4854432b2e414f40797e1364e2ef2bd2
tommasoisi <tommasoisi@gmail.com>
2019-05-14 16:42:17 -0500

    Update analyzeDataVsMCP.cxx

--- TimingAnalysis/analyzeDataVsMCP.cxx
+++ TimingAnalysis/analyzeDataVsMCP.cxx
@@ -141,7 +141,7 @@ int main (int argc, char** argv)
 //   input_file->GetObject("pulse",input_tree);
 
   if (input_tree->GetEntries() < 1000) return 0;
-  else std::cout<<"SELECTED: "<< sensorname << std::endl;
+  else std::cout<<"SELECTED: "<< namesensor << std::endl;
 
   TimingAnalysis example_analyzeData(input_tree);
 
